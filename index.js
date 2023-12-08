1ce5e26ec7ad0aae6b1ae743c9397d21004f9d1e
Chris Gorski <cgorski@gmail.com>
2023-12-07 18:32:12 -0800

    moving to standard index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const cors = require('cors')
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/jobreq-url'), (req, res) => {
+    console.log(`request params ${req.params}`)
+}
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
